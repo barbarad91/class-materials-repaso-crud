0024fa5e796f0708208face9f241f5dea6b77298
Barbara Diaz <barbarad91@gmail.com>
2021-02-10 14:46:05 +0100

    Create models

--- models/coaster.model.js
+++ models/coaster.model.js
@@ -1,6 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-// Aquí el esquema
+const coasterSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    inversions: Number,
+    length: Number,
+    active: Boolean,
+    park: {
+      type: Schema.Types.ObjectId,
+      ref: "Park",
+    },
+  },
+  { timestamps: true }
+)
 
-module.exports = mongoose.model('Coaster', coasterSchema)
+module.exports = mongoose.model("Coaster", coasterSchema)
--- models/park.model.js
+++ models/park.model.js
@@ -1,6 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-// Aquí el esquema
+const parkSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    active: Boolean,
+  },
+  { timestamps: true }
+)
 
-module.exports = mongoose.model('Park', parkSchema)
+module.exports = mongoose.model("Park", parkSchema)
