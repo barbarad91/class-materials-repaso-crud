d8db2c926452fe5d78bf7b83c4b7d2e5f7de09eb
Barbara Diaz <barbarad91@gmail.com>
2021-02-10 19:56:08 +0100

    Display all parks

--- routes/park.routes.js
+++ routes/park.routes.js
@@ -2,6 +2,12 @@ const express = require("express")
 const router = express.Router()
 const Park = require("../models/park.model")
 
+router.get("/", (req, res, next) =>
+  Park.find()
+    .then((parksList) => res.render("parks/parks-index", { parksList }))
+    .catch((err) => next(err))
+)
+
 router.get("/new", (req, res) => res.render("parks/new-park"))
 router.post("/new", (req, res, next) => {
   const { name, description, active } = req.body
