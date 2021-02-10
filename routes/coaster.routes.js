38bfc090a102a612d4988bf0c95ce97f7409fca2
Barbara Diaz <barbarad91@gmail.com>
2021-02-10 16:29:59 +0100

    Display list of available parks in new-coaster view

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -1,7 +1,12 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
+const Park = require("../models/park.model")
+const Coaster = require("../models/coaster.model")
 
-// Aquí los endpoints
-
+router.get("/new", (req, res) => {
+  Park.find()
+    .select("name")
+    .then((parksList) => res.render("coasters/new-coaster", { parksList }))
+})
 
 module.exports = router
